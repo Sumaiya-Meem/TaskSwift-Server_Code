e268f057b98ea341e4472a36145c4aa653ceb40b
Sumaiya-Meem <sumaiyameem.cse6.bu@gmail.com>
2023-12-25 16:51:07 +0600

    add user in database

--- index.js
+++ index.js
@@ -43,6 +43,18 @@ async function run() {
         res.send(result);
     })
 
+    app.post('/users',async(req,res)=>{
+        const user = req.body;
+        const result =await userCollection.insertOne(user);
+        res.send(result)
+    })
+
+    // GET > User
+    app.get('/users',async(req,res)=>{
+        const result =await userCollection.find().toArray();
+        res.send(result);
+    })
+
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
